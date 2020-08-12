0e17ff77fdb5da83d4b1bc50fedbbd8b9c25c280
davidz <davidzn2@gmail.com>
2020-08-12 18:49:12 -0500

    Arreglo de token en general, ya jala y se guara en el storage

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AuthContextProvider  from './contexts/AuthContext'
 // Views
 import Home from "./Components/Home.jsx";
 import Login from "./Components/Login.jsx";
@@ -10,14 +11,15 @@ import UsersList from "./Components/UsersList.jsx";
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/logout" component={Logout} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/users" component={UsersList} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/users" component={UsersList} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
-
